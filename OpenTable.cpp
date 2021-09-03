8ca3c8bceea7e91c602ff775b30b9915925aaca4
Vuniverse0 <v.universe@protonmail.com>
2021-09-03 21:17:02 +0300

    Maube its will be finish

--- OpenTable.cpp
+++ OpenTable.cpp
@@ -24,19 +24,28 @@ int OpenTable::getData( const cords& y , const cords& x , char* buffer )
     byte column = bitIndetificator( y, x ).get();     //generate bit flags to variable flags os type union flags
     setHashTablePosition(column,row,file);            //Set position in hashtable
 
-    position current;
     position temp;
+    //moving from table to data list
+    std::fread( &temp, sizeof( position ), 1, file );
+    std::fseek(file, temp, SEEK_SET);
+    
     cords hash;
     do {        //Check pointer
         std::fread( &temp, sizeof( position ), 1, file );
         std::fread( &hash, sizeof( cords ), 1, file );
         if(hash==cord){
             //Reuse of temp. Other meaning. Sorry ;(
+            //Go to memory
             std::fread( &temp, sizeof( position ), 1, file );
             std::fseek(memory,temp,SEEK_SET);
+
+            //size
             memSize size;
             std::fread( &size, sizeof( memSize ), 1, memory);
+
+            buffer = new char[ size ];
             std::fread( buffer, size, 1, memory);
+            //yea, this is fucking raw pointer. Don`t forgot to clean data ;)
             return size;
         }else {
             std::fseek(file, temp, SEEK_SET);
